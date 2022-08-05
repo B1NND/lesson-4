dd3aead5ee7902d5db3126df9a213f2f9a3e3aa3
xacker <alexandr151215@gmail.com>
2022-08-05 18:06:15 +0300

    1

---
+++ main (1).py
@@ -0,0 +1,23 @@
+import math
+import turtle
+def xt(t):
+  return 6 * math.sin(t) ** 3
+
+def yt(t):
+  return 4 * math.cos(t) -2 * \
+          math.cos(2 * t) - \
+          math.cos(3 * t) - \
+          math.cos(4 * t)
+
+t = turtle.Turtle()
+t.speed(500)
+turtle.colormode(255)
+turtle.Screen().bgcolor(0, 0, 0)
+for i in range(500):
+  t.goto((xt(i) * 10, yt(i) * 20))
+  t.pencolor((255-i) % 255, i % 255, (255 + i)//2%255)
+  t.goto(0, 0)
+
+t.hideturtle()
+turtle.update()
+turtle.mainloop()
